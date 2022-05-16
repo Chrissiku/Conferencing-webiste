69df06d93f894a8b2a5d6ad6f844c3c27ec4f7e4
chrissiku <chrissiku5@gmail.com>
2022-05-16 21:49:20 +0200

    Fix linters errors

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 // Dropdown menu on mobile
-const menubtn = document.querySelector(".menu-icon");
-const mainMenu = document.querySelector(".navbar");
+const menubtn = document.querySelector('.menu-icon');
+const mainMenu = document.querySelector('.navbar');
 
 // function Toogle menu
 function toogleMenu() {
-  menubtn.addEventListener("click", () => {
-    if (menubtn.firstElementChild.classList.contains("fa-bars")) {
-      menubtn.firstElementChild.classList.replace("fa-bars", "fa-times");
-      menubtn.firstElementChild.style.color = "#fff";
-      mainMenu.style.display = "block";
+  menubtn.addEventListener('click', () => {
+    if (menubtn.firstElementChild.classList.contains('fa-bars')) {
+      menubtn.firstElementChild.classList.replace('fa-bars', 'fa-times');
+      menubtn.firstElementChild.style.color = '#fff';
+      mainMenu.style.display = 'block';
     } else {
-      mainMenu.style.display = "none";
-      menubtn.firstElementChild.classList.replace("fa-times", "fa-bars");
-      menubtn.firstElementChild.style.color = "#272a31";
+      mainMenu.style.display = 'none';
+      menubtn.firstElementChild.classList.replace('fa-times', 'fa-bars');
+      menubtn.firstElementChild.style.color = '#272a31';
     }
   });
 }
